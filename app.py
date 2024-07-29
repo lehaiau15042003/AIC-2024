f4947d0a6f8ac1f7bfbabd31802ee8e01ae9d883
Lê Hải Âu <lehaiau15042003@gmail.com>
2024-07-29 16:44:11 +0700

    Update api

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from flask import Flask, request, jsonify, send_from_directory, redirect, render_template, session, url_for
+from flask import Flask, request, jsonify, send_from_directory, redirect, render_template, session, url_for, render_template_string
 from flask_restful import Api, Resource
 import os
-from services.video_service import process_video
+#from services.video_service import process_video
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='webapp/templates', static_folder='webapp/static')
 app.config.from_object('config.Config')
 api = Api(app)
 
 
 @app.route('/')
 def base():
-    return render_template('base.html')
+    return render_template("base.html")
 
-@app.route('/')
+'''@app.route('/')
 def user():
     if "user" in session:
         name = session['user']
     else:
-        return redirect
+        return redirect'''
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -31,7 +31,7 @@ def login():
     return render_template('login.html')
 
 @app.route('/')
-def main():
+def logo():
     pass
 
 class UploadVideo(Resource):
@@ -39,7 +39,7 @@ class UploadVideo(Resource):
         pass
         
 
-class ProcessVideo(Resource):
+'''class ProcessVideo(Resource):
     def post(self):
         data = request.json
         video_path = data.get('file_path')
@@ -49,7 +49,7 @@ class ProcessVideo(Resource):
         return jsonify(result)
 
 api.add_resource(UploadVideo, '/upload')
-api.add_resource(ProcessVideo, '/process')
+api.add_resource(ProcessVideo, '/process')'''
 
 if __name__ == '__main__':
     app.run(debug=True)
