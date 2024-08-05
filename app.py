78dc1c168d6c58ada65a47969fea5746c605a7ab
Lê Hải Âu <lehaiau15042003@gmail.com>
2024-08-05 14:38:35 +0700

    Update api_2

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile, Header
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel
+from pathlib import Path
 
 app = FastAPI()
 
@@ -12,4 +14,17 @@ app.mount("/static", StaticFiles(directory="webapp/static"), name="static")
 async def root(request: Request):
     return templates.TemplateResponse("base.html", {"request":request})
 
-
+@app.post("/upload")
+async def upload_video(    
+    request : Request,
+    page_id : str,
+    file : UploadFile = File(...),
+    access_token : str = Header(...),
+    ):
+    
+    file_location = Path("webapp/static/upload")
+    
+    with open(file_location, "wb") as f:
+        f.write(file.file.read())
+    
+    return JSONResponse(content={"filename": file.filename, "location": str(file_location)})
