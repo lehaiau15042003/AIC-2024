a63586a676c586ca3f77630d7404a6dfd4dcb312
Lê Hải Âu <lehaiau15042003@gmail.com>
2024-08-06 13:10:40 +0700

    Update api_3

--- webapp/static/js/script.js
+++ webapp/static/js/script.js
@@ -1,19 +1,76 @@
-document.getElementById('login-form').addEventListener('submit', function(event)
-{
+document.getElementById('upload-link').addEventListener('click', function(event) {
     event.preventDefault();
+    document.getElementById('file-input').click();
+});
 
-    const username = document.getElementById('border_user').value;
+document.getElementById('file-input').addEventListener('change', function(event){
+    const file = event.target.files[0];
+    if (file) {
+        console.log('File selected: ', file.name);
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const imagePreview = document.getElementById('image-preview');
+            const videoPreview = document.getElementById('video-preview');
+            if (file.type.startsWith('image/')){
+                imagePreview.src = e.target.result;
+                imagePreview.style.display = "block";
+                videoPreview.style.display = 'none';
+            } else if (file.type.startsWith('video/')) {
+                videoPreview.src = e.target.result;
+                videoPreview.style.display = 'block';
+                imagePreview.style.display = 'none';
+            }
+            
+        }
 
-    const password = document.getElementById('border_pass').value;
-
-    const errorMessage = document.getElementById('error-message');
-
-    if (username == "admin" && password == "admin"){
-        alert("Login Success");
-        window.location.href = "base.html";
+        reader.readAsDataURL(file);
     }
-    else{
-        errorMessage.textContent = "Invalid username or password";
-        errorMessage.style.display = "block";
+});
+
+document.getElementById('upload-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const fileInput = document.getElementById('file-input');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onloadend = function() {
+            const base64String = reader.result.split(',')[1];
+
+            const jsonData = JSON.stringify({
+                fileName: file.name,
+                fileType: file.type,
+                fileContent: base64String
+            });
+
+            fetch('/upload', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: jsonData
+            })
+            .then(response => {
+                if (response.redirected) {
+                    window.location.href = response.url;
+                } else {
+                    return response.json().then(data => {
+                        if (data.message === 'File uploaded successfully') {
+                            window.location.href = 'upload/result?file_url=${encodeURIComponent(data.file_url)}';
+                        } else {
+                            alert("Upload Failed: " + data.error);
+                        }
+                    })
+                }
+            })
+            
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Upload Failed: ' + error.message);
+            });
+        };
+        reader.readAsDataURL(file);
+    } else {
+        alert('Please select a file to upload.');
     }
 });
